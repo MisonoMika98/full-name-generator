d6aa12c27d347ab82c422a292640ccb2a4f98eed
ariel raymundo <ariraymundo98@gmail.com>
2026-04-14 10:19:24 -0700

    cleaned up spacing

--- src/main/java/com/pluralsight/FullNameGenerator.java
+++ src/main/java/com/pluralsight/FullNameGenerator.java
@@ -9,8 +9,10 @@ public class FullNameGenerator
         Scanner input = new Scanner(System.in);
 
         // name of program
+        System.out.println();
         System.out.println("Full Name Generator");
         System.out.println("--------------------------------------");
+
 
         // asks user for first name
         System.out.print("Please Enter Your First Name: ");
@@ -21,6 +23,7 @@ public class FullNameGenerator
         System.out.print("Please Enter Your Middle Name or Initial (Press Enter to SKIP): ");
         String middleName = input.nextLine().trim();
 
+
         // asks user to input last name
         System.out.print("Please Enter Your Last Name: ");
         String lastName = input.nextLine().trim();
@@ -30,6 +33,7 @@ public class FullNameGenerator
         System.out.print("Please Enter Your Suffix if you have one (Press Enter to SKIP): ");
         String haveSuffix = input.nextLine().trim();
 
+
         // program prints individual parts first
         System.out.println();
         System.out.println("First Name: " + firstName);
@@ -37,8 +41,10 @@ public class FullNameGenerator
         System.out.println("Last Name: " + lastName);
         System.out.println("Suffix: " + haveSuffix);
 
+
         // defines fullName as user's first name to check for other inputs
         String fullName = firstName;
+
 
         // checks if user input anything for middle name/initial, if not adds a space for formatting the print output
         if (middleName.length() > 0)
@@ -46,13 +52,17 @@ public class FullNameGenerator
             fullName += " " + middleName;
         }
 
+
         // adds user's last name no matter what, and a space for formatting. after checking for middle name/initial
         fullName += " " + lastName;
+
 
         // checks if user input anything for suffix, includes comma and space for formatting if true
         if (haveSuffix.length() > 0) {
             fullName += ", " + haveSuffix;
         }
+
+
 
         // final output + formatting
         System.out.println("------------------------------------------");
